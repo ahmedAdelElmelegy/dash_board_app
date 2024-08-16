d90e5db0b5c36f49b61fe1a2945cf9ece3b801db
ahmed adel <ahmedadelelmelegy2@gmail.com>
2024-08-16 14:08:11 +0300

    add asets and generate this assets

---
+++ lib/constants/assets.dart
@@ -0,0 +1,56 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesImg
+  /// assets/images/img.svg
+  static const String imagesImg = "assets/images/img.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesIncomecircle
+  /// assets/images/incomecircle.svg
+  static const String imagesIncomecircle = "assets/images/incomecircle.svg";
+
+  /// Assets for imagesJosua
+  /// assets/images/josua.svg
+  static const String imagesJosua = "assets/images/josua.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+
+  /// Assets for imagesMadrani
+  /// assets/images/madrani.svg
+  static const String imagesMadrani = "assets/images/madrani.svg";
+
+  /// Assets for imagesMyCard
+  /// assets/images/my_card.svg
+  static const String imagesMyCard = "assets/images/my_card.svg";
+
+  /// Assets for imagesMyInvestments
+  /// assets/images/my_investments.svg
+  static const String imagesMyInvestments = "assets/images/my_investments.svg";
+
+  /// Assets for imagesMytransaction
+  /// assets/images/mytransaction.svg
+  static const String imagesMytransaction = "assets/images/mytransaction.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesWalletAccount
+  /// assets/images/wallet_account.svg
+  static const String imagesWalletAccount = "assets/images/wallet_account.svg";
+}
+
